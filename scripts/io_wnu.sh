1bf9c09a8d743a304aa085e55d8565dad6376590
envieid0c <envieidoc@gmail.com>
2016-07-06 15:15:58 +0300

    - fix service "check wi-fi" switch service

--- scripts/io_wnu.sh
+++ scripts/io_wnu.sh
@@ -6,28 +6,49 @@ POPUP=/usr/local/sbin/io_wnu_popup
 SERVICE=`launchctl list | grep wnu | awk '{print $2}'`
 
 # check service status
-if [ "$SERVICE" != "0" ]; then
-  echo "Service disabled" > /tmp/check_service
+if [ "$SERVICE" = "0" ]; then
+  echo "Service Enabled" > "$CONF"check_service
+  echo "Enabled" > "$CONF"service
+  echo "0" > "$CONF"service_status
 else
-  echo "Service enabled" > /tmp/check_service
+  echo "Service Disabled" > "$CONF"check_service
+  echo "Disabled" > "$CONF"service
+  echo "1" > "$CONF"service_status
 fi
 
 CHECK_SERVICE=$(cat /tmp/check_service)
 
 function switch_service {
-  launchctl list | grep wnu | awk '{print $2}' > /tmp/io_service
+  launchctl list | grep wnu | awk '{print $2}' > "$CONF"service_status
 
   exec 6<&0
-  exec < /tmp/io_service
+  exec < "$CONF"service_status
   read a1
 
   if [ "$a1" != "0" ]; then
-      launchctl load /Library/LaunchAgents/io_wnu.plist 
-      "$POPUP" -title ' Service enabled' -message '' -timeout 3
-      rm -rf /tmp/io_service
+    launchctl load /Library/LaunchAgents/io_wnu.plist
+    "$POPUP" -title ' Service enabled' -message '' -timeout 3
+    echo "Service Enabled" > "$CONF"check_service
+    echo "Enabled" > "$CONF"service
+    echo "0" > "$CONF"service_status
   else
-      launchctl unload /Library/LaunchAgents/io_wnu.plist
-      "$POPUP" -title ' Service disabled' -message '' -timeout 3
+    launchctl unload /Library/LaunchAgents/io_wnu.plist
+    "$POPUP" -title ' Service disabled' -message '' -timeout 3
+    echo "Service Disabled" > "$CONF"check_service
+    echo "Disabled" > "$CONF"service
+    echo "1" > "$CONF"service_status
+  fi
+}
+
+function switch_utility {
+  unit=$(cat "$CONF"utility)
+
+  if [ "$unit" != "Enabled" ]; then
+    echo Enabled > "$CONF"utility
+    osascript -e 'open app "StatusBarApp"' & "$POPUP" -title 'Shown Utility' -message '' -timeout 3
+  else
+    echo Disabled > "$CONF"utility
+    osascript -e 'quit app "StatusBarApp"' & "$POPUP" -title 'Hidden Utility' -message '' -timeout 3
   fi
 }
 
@@ -39,7 +60,7 @@ while [ true ]; do
   exec 6<&0
   exec < /Library/Application\ Support/WLAN/com.realtek.utility.wifi/MAC
   read a1
-  StatusBarApp_POPUP="$(/usr/local/sbin/io_wnu_popup -title 'Wi-Fi Disabled' -subtitle "$CHECK_SERVICE" -message 'Actions?' -actions 'Enable Wi-Fi',"Switch Service","Show Utility","Hide Utility" -timeout 30 -appIcon /Library/Application\ Support/WLAN/StatusBarApp.app/Contents/Resources/ModelIcon.icns)"
+  StatusBarApp_POPUP="$(/usr/local/sbin/io_wnu_popup -title 'Wi-Fi Disabled' -subtitle "$CHECK_SERVICE" -message 'Actions?' -actions 'Enable Wi-Fi',"Switch Service","Show/Hide Utility" -timeout 30 -appIcon /Library/Application\ Support/WLAN/StatusBarApp.app/Contents/Resources/ModelIcon.icns)"
   #-sound default)
       case $StatusBarApp_POPUP in
         "@TIMEOUT") `pkill -f ~/.io_wnuup` ;;
@@ -48,14 +69,13 @@ while [ true ]; do
         "@ACTIONCLICKED") echo "You clicked the alert default action button" ;;
         "Enable Wi-Fi") osascript -e 'quit app "StatusBarApp"' ; echo "1" > "$a1" ; echo "0" > "$a1" ; open -a /Library/Application\ Support/WLAN/StatusBarApp.app ;;
         "Switch Service") switch_service ;;
-        "Show Utility") osascript -e 'quit app "StatusBarApp"' ; echo "1" > "$a1" ; echo "0" > "$a1" ; open -a /Library/Application\ Support/WLAN/StatusBarApp.app ;;
-        "Hide Utility") osascript -e 'quit app "StatusBarApp"' ; echo "1" > "$a1" ; pkill -f ~/.io_wnuup ;;
+        "Show/Hide Utility") switch_utility ;;
         **) echo "? --> $StatusBarApp_POPUP" ;;
       esac
   fi
   if [ "$result" = disconnected -a "$errorscount" = 0 ]; then
   result=connected
-  StatusBarApp_POPUP="$(/usr/local/sbin/io_wnu_popup -title 'Wi-Fi Enabled' -subtitle "$CHECK_SERVICE" -message 'All is well?' -actions "Disable Wi-Fi","Switch Service","Show Utility","Hide Utility" -timeout 15 -appIcon /Library/Application\ Support/WLAN/StatusBarApp.app/Contents/Resources/ModelIcon.icns)"
+  StatusBarApp_POPUP="$(/usr/local/sbin/io_wnu_popup -title 'Wi-Fi Enabled' -subtitle "$CHECK_SERVICE" -message 'All is well?' -actions "Disable Wi-Fi","Switch Service","Show/Hide Utility" -timeout 15 -appIcon /Library/Application\ Support/WLAN/StatusBarApp.app/Contents/Resources/ModelIcon.icns)"
   #-sound default)
       case $StatusBarApp_POPUP in
         "@TIMEOUT") echo "OK" ;;
@@ -64,8 +84,7 @@ while [ true ]; do
         "@ACTIONCLICKED") echo "You clicked the alert default action button" ;;
         "Disable Wi-Fi") osascript -e 'quit app "StatusBarApp"' ; echo "1" > $a1 ; open -a /Library/Application\ Support/WLAN/StatusBarApp.app ;;
         "Switch Service") switch_service ;;
-        "Show Utility") osascript -e 'quit app "StatusBarApp"' ; echo "1" > "$a1" ; echo "0" > "$a1" ; open -a /Library/Application\ Support/WLAN/StatusBarApp.app ;;
-        "Hide Utility") osascript -e 'quit app "StatusBarApp"' ; echo "1" > $a1 ; pkill -f ~/.io_wnuup ;;
+        "Show/Hide Utility") switch_utility ;;
         **) echo "? --> $StatusBarApp_POPUP" ;;
       esac
   fi
